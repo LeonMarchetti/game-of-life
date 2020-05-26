461d4d8e3ed47fe58640161c940e2a1b77095cc7
LeonMarchetti <mleonardoa@gmail.com>
2020-05-26 19:02:04 -0300

    Pensando en celulaestado

--- ideas/celulaestado.py
+++ ideas/celulaestado.py
@@ -1,44 +1,54 @@
-class CelulaEstado(object):
+class CelulaEstado:
     @staticmethod
     def avanzar(vecinos_vivos):
         pass
 
     @staticmethod
     def dibujar():
+        pass
+
+    @staticmethod
+    def conteo_vecino():
         pass
 
 class CelulaEstadoVivo(CelulaEstado):
     @staticmethod
     def avanzar(vecinos_vivos):
-        super().avanzar(vecinos_vivos)
         if 2 > vecinos_vivos > 3:
-            return CelulaEstadoMuerto()
-        return None
+            return CelulaEstadoMuerto
+        return CelulaEstadoVivo
 
     @staticmethod
     def dibujar():
         return "█"
 
+    @staticmethod
+    def conteo_vecino():
+        return 1
+
 
 class CelulaEstadoMuerto(CelulaEstado):
     @staticmethod
     def avanzar(vecinos_vivos):
-        super().avanzar(vecinos_vivos)
         if vecinos_vivos == 3:
-            return CelulaEstadoVivo()
-        return None
+            return CelulaEstadoVivo
+        return CelulaEstadoMuerto
 
     @staticmethod
     def dibujar():
         return "."
+
+    @staticmethod
+    def conteo_vecino():
+        return 0
 
 
 class CelulaEstadoFactory(object):
     @staticmethod
     def obtener(estado):
         if estado == 0:
-            return CelulaEstadoMuerto()
+            return CelulaEstadoMuerto
         elif estado == 1:
-            return CelulaEstadoVivo()
+            return CelulaEstadoVivo
         else:
-            return CelulaEstado()
+            return CelulaEstado
